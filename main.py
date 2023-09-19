49445a518b8726a81e8ccf1965e2381a5eecc19b
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2023-09-19 20:40:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+a = int(input("Введіть день тиждня :"))
+if a==1:
+    print("Понеділок")
+elif a==2:
+    print("Вівторок")
+elif a==3:
+    print("Середа")
+elif a==4:
+    print("Четвер")
+elif a==5:
+    print("П'ятниця")
+elif a==6:
+    print("Субота")
+elif a==7:
+    print("Неділя")
+else:
+    print("Error")
+
+
+
+
